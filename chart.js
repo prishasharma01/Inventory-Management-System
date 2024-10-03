721baeeabf54c6fdece4cbd4814e85d377b79ab5
Prisha Sharma <156198799+prishasharma01@users.noreply.github.com>
2024-10-03 20:04:25 +0530

    Add files via upload

---
+++ chart.js
@@ -0,0 +1,35 @@
+var ctx = document.getElementById('linechart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June','Jul','Aug','Sep','Oct','Nov','Dec'],
+        datasets: [{
+            label: '# of sale',
+            data: [12000,45000,9000,20000,30000,25000,33000,19000,35000,28000,43000],
+            backgroundColor: [
+                'rgb(255, 196, 0)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(214, 135, 16, 0.815)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
---
+++ chart2.js
@@ -0,0 +1,31 @@
+var cntx = document.getElementById('bargraph').getContext('2d');
+const myCharts = new Chart(cntx, {
+    type: 'pie',
+    data: {
+        labels: ['Online','Offline','Marketing'],
+        datasets: [{
+            label: '# of sale',
+            data: [2135,1987,1734],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+       responsive:true
+    }
+});
